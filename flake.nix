02580e5df19ee9ec4cee5b3d941a3eb85c0849b4
Tetsuya81 <tetsuya8109@gmail.com>
2026-02-09 16:20:51 +0900

    First commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Minimum Viewer - Rust CUI file viewer development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            clippy
+            rust-analyzer
+          ];
+        };
+      });
+}
